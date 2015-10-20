99bd7fd9a0f54bab3cd7584cbb32f76f8e60145a
nhokquay9x26 <daoquocviet11i3@gmail.com>
2015-10-20 14:40:38 +0700

    Update file LeageActivity, create layout activity_leage and update values

--- app/src/main/java/vn/asiantech/internship/footballmanager/ui/leage/LeageActivity.java
+++ app/src/main/java/vn/asiantech/internship/footballmanager/ui/leage/LeageActivity.java
@@ -1,7 +1,15 @@
 package vn.asiantech.internship.footballmanager.ui.leage;
 
+import android.app.Activity;
+
+import org.androidannotations.annotations.EActivity;
+
+import vn.asiantech.internship.footballmanager.R;
+
 /**
  * Created by nhokquay9x26 on 20/10/15.
  */
-public class LeageActivity {
+@EActivity(R.layout.activity_leage)
+public class LeageActivity extends Activity {
+
 }
