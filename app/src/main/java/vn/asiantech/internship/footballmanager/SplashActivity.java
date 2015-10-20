74c5723e888b8debcac8c541615ac1d981f8fa95
nhokquay9x26 <daoquocviet11i3@gmail.com>
2015-10-20 14:58:13 +0700

    delete file java and edit file MainActicity to SplashActicity

---
+++ app/src/main/java/vn/asiantech/internship/footballmanager/SplashActivity.java
@@ -0,0 +1,11 @@
+package vn.asiantech.internship.footballmanager;
+
+import android.app.Activity;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_splash)
+public class SplashActivity extends Activity {
+
+
+}
--- app/src/main/java/vn/asiantech/internship/footballmanager/ui/player/PlayerActivity.java
+++
@@ -1,7 +0,0 @@
-package vn.asiantech.internship.footballmanager.ui.player;
-
-/**
- * Created by nhokquay9x26 on 20/10/15.
- */
-public class PlayerActivity {
-}
