59514ad2ad6499841db91ed9635e28350058aa92
nhokquay9x26 <daoquocviet11i3@gmail.com>
2015-10-21 15:45:29 +0700

    update LeagueActivity,  LeagueAdapter and create layout dialog_add_league, update values, add library animation to build.gradle

--- app/src/main/java/vn/asiantech/internship/footballmanager/ui/league/LeagueActivity.java
+++ app/src/main/java/vn/asiantech/internship/footballmanager/ui/league/LeagueActivity.java
@@ -1,9 +1,19 @@
 package vn.asiantech.internship.footballmanager.ui.league;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.gitonway.lee.niftymodaldialogeffects.lib.Effectstype;
+import com.gitonway.lee.niftymodaldialogeffects.lib.NiftyDialogBuilder;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
@@ -22,10 +32,12 @@ import vn.asiantech.internship.footballmanager.model.LeagueItem;
 @EActivity(R.layout.activity_league)
 public class LeagueActivity extends Activity implements LeagueAdapter.OnItemListener {
 
+    private Effectstype effect;
     List<LeagueItem> mLeagues;
     LeagueAdapter mAdapter;
     RecyclerView mRecyclerView;
     LinearLayoutManager mLinearLayoutManager;
+    ScaleInAnimationAdapter scaleAdapter;
 
     @AfterViews
     void afterView() {
@@ -38,22 +50,53 @@ public class LeagueActivity extends Activity implements LeagueAdapter.OnItemList
         mRecyclerView.setLayoutManager(mLinearLayoutManager);
 
         AlphaInAnimationAdapter alphaAdapter = new AlphaInAnimationAdapter(mAdapter);
-        ScaleInAnimationAdapter scaleAdapter = new ScaleInAnimationAdapter(alphaAdapter);
+        scaleAdapter = new ScaleInAnimationAdapter(alphaAdapter);
         scaleAdapter.setDuration(500);
         scaleAdapter.setFirstOnly(false);
         mRecyclerView.setAdapter(scaleAdapter);
     }
 
     @Click(R.id.imgAdd)
-    void addData() {
-        for (int i = 0; i < 50; i++) {
-            LeagueItem leagueItem = new LeagueItem("League " + i);
-            leagueItem.save();
-        }
-        mAdapter.notifyDataSetChanged();
+    void addData(View v) {
+        customDialog(v);
+    }
+
+    public void customDialog(View v) {
+        effect = Effectstype.SlideBottom;
+
+        final NiftyDialogBuilder dialogBuilder = NiftyDialogBuilder.getInstance(this);
+        dialogBuilder
+                .isCancelableOnTouchOutside(false)
+                .withTitle("Add League")
+                .withTitleColor("#000000")
+                .withEffect(effect)
+                .withButton1Text("Cancel")
+                .withDialogColor("#0099FF")
+                .withButton2Text("Ok")
+                .setCustomView(R.layout.dialog_add_league, v.getContext())
+                .setButton1Click(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialogBuilder.dismiss();
+                    }
+                })
+                .setButton2Click(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        EditText edtName = (EditText) dialogBuilder.findViewById(R.id.edtName);
+                        String name = edtName.getText().toString();
+                        LeagueItem leagueItem = new LeagueItem(name);
+                        leagueItem.save();
+                        mLeagues.add(leagueItem);
+                        scaleAdapter.notifyDataSetChanged();
+                        dialogBuilder.dismiss();
+                    }
+                })
+                .show();
     }
 
     @Override
     public void onItemClick(int position) {
+
     }
 }
