0fe586626d02066ea8ca1197cbce2df4ab9c99bd
nhokquay9x26 <daoquocviet11i3@gmail.com>
2015-10-22 00:43:23 +0700

    create file Item, Activity, Adapter of FootBallTeam and update ui league, model and layout

---
+++ app/src/main/java/vn/asiantech/internship/footballmanager/common/Utils.java
@@ -0,0 +1,8 @@
+package vn.asiantech.internship.footballmanager.common;
+
+/**
+ * Created by nhokquay9x26 on 21/10/15.
+ */
+public class Utils {
+    public static final String EXTRA_KEY_ = "";
+}
